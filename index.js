d8f61eb9811e2eec4cf4c82d53a72fd9f6051721
Madhu2112 <kothamadhuvani@gmail.com>
2025-01-05 23:53:50 +0530

    new file:   .gitignore 	new file:   MOCK_DATA.json 	new file:   index.js 	new file:   package-lock.json 	new file:   package.json 	new file:   task.txt

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const users = require('./MOCK_DATA.json');
+const app = express()
+const PORT = 8000;
+
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+});
+app.get('/users', (req, res) => {
+    const html = `
+    <ul>
+        ${users.map((user) => `<li> ${user.first_name}</li>`)}
+    </ul>
+    `
+    res.send(html);
+})
+app.listen(PORT, () => console.log(`server started at port ${PORT}`));
